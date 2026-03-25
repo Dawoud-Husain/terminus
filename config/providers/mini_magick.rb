644c26d47be89f14f8fbca50aca04c45485c298f
Brooke Kuhlmann <brooke@alchemists.io>
2026-03-25 13:04:57 -0600

    Updated MiniMagick configuration to be more strict
    Ensures MiniMagick errors on non-zero exit status while forwarding all warnings to errors as well. This also leverages the existing application `tmp` directory for processing images and restricts environment access for improved security.
    
    Milestone: minor

--- config/providers/mini_magick.rb
+++ config/providers/mini_magick.rb
@@ -1,10 +1,20 @@
 # frozen_string_literal: true
 
+require "refinements/pathname"
+
+using Refinements::Pathname
+
 Hanami.app.register_provider :mini_magick, namespace: true do
   prepare { require "mini_magick" }
 
   start do
-    MiniMagick.configure { |config| config.logger = slice[:logger] }
+    MiniMagick.configure do |config|
+      config.errors = true
+      config.warnings = true
+      config.restricted_env = true
+      config.tmpdir = slice.root.join("tmp/mini_magick").make_dir
+      config.logger = slice[:logger]
+    end
 
     register :core, MiniMagick
     register :image, MiniMagick::Image
