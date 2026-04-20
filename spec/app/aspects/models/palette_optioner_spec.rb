75587d1d02211093745ef9f4ab7f1fc35f2ee853
Brooke Kuhlmann <brooke@alchemists.io>
2026-04-20 16:11:46 -0600

    Added model palette optioner
    Provides a specialized select option builder for acquiring palettes associated with a model. In the situation, like a custom model that isn't part of the official TRMNL API and no palettes are associated with a model, then the full list of palettes will be provided.
    
    This will initially be used when rendering information within the model views.
    
    Issue: 303
    Milestone: minor

---
+++ spec/app/aspects/models/palette_optioner_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+
+RSpec.describe Terminus::Aspects::Models::PaletteOptioner, :db do
+  subject(:optioner) { described_class.new }
+
+  describe "#call" do
+    let(:model) { Factory[:model] }
+    let(:palette_a) { Factory[:palette] }
+    let(:palette_b) { Factory[:palette] }
+
+    before do
+      palette_a
+      palette_b
+    end
+
+    it "answers only palettes associated with model" do
+      Factory[:model_palette, model_id: model.id, palette_id: palette_a.id]
+
+      expect(optioner.call(model)).to eq([["Select...", ""], [palette_a.name, palette_a.id]])
+    end
+
+    it "answers all palettes when model isn't provided" do
+      expect(optioner.call).to eq(
+        [
+          ["Select...", ""],
+          [palette_a.name, palette_a.id],
+          [palette_b.name, palette_b.id]
+        ]
+      )
+    end
+
+    it "answers all palettes when there are no associations" do
+      expect(optioner.call(model)).to eq(
+        [
+          ["Select...", ""],
+          [palette_a.name, palette_a.id],
+          [palette_b.name, palette_b.id]
+        ]
+      )
+    end
+  end
+end
